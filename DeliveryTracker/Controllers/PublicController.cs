0d8e7750b503c714bb9b804f3e46a625d83c7a49
eiu165 <eiu165@gmail.com>
2012-06-08 14:00:26 -0500

    look at stuff

--- DeliveryTracker/Controllers/PublicController.cs
+++ DeliveryTracker/Controllers/PublicController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.Linq;
 using System.Text;
@@ -27,8 +28,33 @@ namespace DeliveryTracker.Controllers
             }
 
             ViewBag.ConnString = s;
+
+            ViewBag.AppSettings = PublicController.ReadAppSettings();
+
+
             return View();
         }
 
+
+        // Get the AppSettings section.        
+        // This function uses the AppSettings property
+        // to read the appSettings configuration 
+        // section.
+        public static string ReadAppSettings()
+        {
+            var s = new StringBuilder();
+            // Get the AppSettings section.
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+
+            if (appSettings.Count == 0)
+            {
+                s.Append("AppSettings is empty Use GetSection command first.");
+            }
+            for (int i = 0; i < appSettings.Count; i++)
+            {
+                s.AppendFormat("#{0} Key: <b>{1}</b> Value: <b>{2}</b> <br />", i, appSettings.GetKey(i), appSettings[i]);
+            }
+            return s.ToString();
+        }
     }
 }
